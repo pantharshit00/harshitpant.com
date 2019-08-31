b99aeb868cbf07912079bef1295d36dad43c5482
Harshit Pant <pantharshit00@gmail.com>
2019-08-31 21:25:26 +0530

    setup netilfy cache

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,7 +73,8 @@ module.exports = {
         linkImagesToOriginal: false
       }
     },
-    "gatsby-plugin-twitter"
+    "gatsby-plugin-twitter",
+    "gatsby-plugin-netlify-cache"
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
