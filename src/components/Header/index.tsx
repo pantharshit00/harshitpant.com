d1325303897cfe33095ab403ae6d1eba3f7d1363
Harshit Pant <pantharshit00@gmail.com>
2019-08-31 17:32:41 +0530

    make header look good on ultrawide

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,18 +3,25 @@ import * as L from './styles';
 
 const Header: React.FC = () => {
   return (
-    <L.Header>
-      <div>
-        <L.Logo>hP</L.Logo>
-      </div>
-      <L.Nav>
-        <L.NavA href="#contact">Contact</L.NavA>
-        <L.NavA href="/blog">Blog</L.NavA>
-        <L.NavA href="#about">About</L.NavA>
-        <L.NavA href="/resume">Resume</L.NavA>
-      </L.Nav>
-      <L.MenuButton>Menu</L.MenuButton>
-    </L.Header>
+    <div
+      style={{
+        margin: '0 auto',
+        maxWidth: '1550px',
+      }}
+    >
+      <L.Header>
+        <div>
+          <L.Logo>hP</L.Logo>
+        </div>
+        <L.Nav>
+          <L.NavA href="#contact">Contact</L.NavA>
+          <L.NavA href="/blog">Blog</L.NavA>
+          <L.NavA href="#about">About</L.NavA>
+          <L.NavA href="/resume">Resume</L.NavA>
+        </L.Nav>
+        <L.MenuButton>Menu</L.MenuButton>
+      </L.Header>
+    </div>
   );
 };
 
